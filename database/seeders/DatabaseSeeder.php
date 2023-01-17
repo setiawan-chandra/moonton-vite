23593002214c241bb994e1707ca26a1d8b04c9dd
schatz wawan <schatzwawan@gmail.com>
2023-01-17 08:36:07 +0700

    Feat: Setup Spatie

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,5 +20,9 @@ class DatabaseSeeder extends Seeder
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        $this->call(RoleTableSeeder::class);
+        $this->call(UserTableSeeder::class);
+
     }
 }
