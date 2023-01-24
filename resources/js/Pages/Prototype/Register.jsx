aa19713184ecac93f20254e8cf2f278e218c135e
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-24 17:27:23 +0700

    Feat: Slicing Sign-Up

--- resources/js/Pages/Prototype/Register.jsx
+++ resources/js/Pages/Prototype/Register.jsx
@@ -1,14 +1,13 @@
 import InputLabel from "@/Components/InputLabel";
 import PrimaryButton from "@/Components/PrimaryButton";
 import TextInput from "@/Components/TextInput";
-import { Head, Link } from "@inertiajs/inertia-react";
+import { Link, Head } from "@inertiajs/inertia-react";
 import React from "react";
 
-export default function Login() {
+export default function Register() {
     return (
         <>
             <Head title="Sign Up" />
-
             <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
                 <div className="fixed top-[-50px] hidden lg:block">
                     <img
@@ -22,7 +21,7 @@ export default function Login() {
                         <img src="/images/moonton-white.svg" alt="" />
                         <div className="my-[70px]">
                             <div className="font-semibold text-[26px] mb-3">
-                                Welcome Back
+                                Sign Up
                             </div>
                             <p className="text-base text-[#767676] leading-7">
                                 Explore our new movies and get <br />
@@ -33,13 +32,26 @@ export default function Login() {
                             <div className="flex flex-col gap-6">
                                 <div>
                                     <InputLabel
+                                        forInput="fullname"
+                                        value="Full Name"
+                                    />
+                                    <TextInput
+                                        type="text"
+                                        name="fullname"
+                                        placeholder="Your fullname..."
+                                        defaultValue="Setiawan Chandra"
+                                    />
+                                </div>
+                                <div>
+                                    <InputLabel
                                         forInput="email"
                                         value="Email Address"
                                     />
                                     <TextInput
                                         type="email"
                                         name="email"
-                                        placeholder="Email Address"
+                                        placeholder="Your Email Address"
+                                        defaultValue="setiawan.chandra@gmail.com"
                                     />
                                 </div>
                                 <div>
@@ -50,23 +62,20 @@ export default function Login() {
                                     <TextInput
                                         type="password"
                                         name="password"
-                                        placeholder="Password"
+                                        placeholder="Your Password"
                                     />
                                 </div>
                             </div>
                             <div className="grid space-y-[14px] mt-[30px]">
                                 <PrimaryButton>
                                     <span className="text-base font-semibold">
-                                        Start Watching
+                                        Sign Up
                                     </span>
                                 </PrimaryButton>
-                                <Link href={route("prototype.register")}>
-                                    <PrimaryButton
-                                        type="button"
-                                        variant="light-outline"
-                                    >
+                                <Link href={route("prototype.login")}>
+                                    <PrimaryButton variant="light-outline">
                                         <span className="text-base text-white">
-                                            Create New Account
+                                            Sign In to My Account
                                         </span>
                                     </PrimaryButton>
                                 </Link>
