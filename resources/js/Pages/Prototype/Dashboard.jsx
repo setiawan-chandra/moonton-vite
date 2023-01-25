e09325317548f7b600df7adaf7fe16d4d8abe82f
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-25 17:22:04 +0700

    Feat: Slicing Featured Movies - Install flickity for slider featured movies: "npm i react-flickity-component" and "npm i flickity"

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -1,5 +1,43 @@
 import Authenticated from "@/Layouts/Authenticated/Index";
+import Flickity from "react-flickity-component";
+import { Head } from "@inertiajs/inertia-react";
+import FeaturedMovie from "@/Components/FeaturedMovie";
 
 export default function Dashboard() {
-    return <Authenticated />;
+    const flickityOptions = {
+        cellAlign: "left",
+        contain: true,
+        groupCells: 1,
+        wrapAround: false,
+        pageDots: false,
+        prevNextButtons: false,
+        draggable: ">1",
+    };
+    return (
+        <Authenticated>
+            <Head>
+                <link
+                    rel="stylesheet"
+                    href="https://unpkg.com/flickity@2/dist/flickity.min.css"
+                />
+            </Head>
+            <div>
+                <div className="font-semibold text-[22px] text-black mb-4">
+                    Featured Movies
+                </div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+                    {[1, 2, 3, 4].map((i) => (
+                        <FeaturedMovie
+                            key={i}
+                            slug="the-batman-in-love"
+                            name={`The Batman In Love ${i}`}
+                            category="Comedy"
+                            thumbnail="https://picsum.photos/id/1/300/300"
+                            rating={i + 1}
+                        />
+                    ))}
+                </Flickity>
+            </div>
+        </Authenticated>
+    );
 }
