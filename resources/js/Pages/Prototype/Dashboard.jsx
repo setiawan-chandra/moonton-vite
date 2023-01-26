e274582fa66f80cf043bf0b7ecd6265d3abf4070
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-26 11:08:15 +0700

    Feat: Slicing Movie Card Component

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -2,6 +2,7 @@ import Authenticated from "@/Layouts/Authenticated/Index";
 import Flickity from "react-flickity-component";
 import { Head } from "@inertiajs/inertia-react";
 import FeaturedMovie from "@/Components/FeaturedMovie";
+import MovieCard from "@/Components/MovieCard";
 
 export default function Dashboard() {
     const flickityOptions = {
@@ -46,41 +47,13 @@ export default function Dashboard() {
                 </div>
                 <Flickity className="gap-[30px]" options={flickityOptions}>
                     {[1, 2, 3, 4, 5, 6].map((i) => (
-                        <div
-                            className="absolute group overflow-hidden mr-[30px]"
+                        <MovieCard
                             key={i}
-                        >
-                            <img
-                                src="/images/browse-1.png"
-                                className="object-cover rounded-[30px] h-[340px] w-[250px]"
-                                alt=""
-                            />
-                            <div className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
-                                <div className="px-7 pb-7">
-                                    <div className="font-medium text-xl text-white">
-                                        Meong Golden
-                                    </div>
-                                    <p className="mb-0 text-gray-300 text-base mt-[10px]">
-                                        Horror • Love
-                                    </p>
-                                </div>
-                            </div>
-                            <div
-                                className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2
-                            -translate-x-1/2 z-20 transition ease-in-out duration-500"
-                            >
-                                <img
-                                    src="/icons/ic_play.svg"
-                                    className=""
-                                    width="50"
-                                    alt=""
-                                />
-                            </div>
-                            <a
-                                href="watching.html"
-                                className="inset-0 absolute z-50"
-                            ></a>
-                        </div>
+                            slug="the-batman-in-love"
+                            name={`The Batman In Love ${i}`}
+                            category="Comedy"
+                            thumbnail="https://picsum.photos/id/1/300/300"
+                        />
                     ))}
                 </Flickity>
             </div>
