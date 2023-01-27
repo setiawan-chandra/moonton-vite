b3108df9343126b6a8da8380944e5a7b455e79c7
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-27 15:13:33 +0700

    Feat: Integration Migration User Subscription

---
+++ database/migrations/2023_01_27_072544_create_user_subsciptions_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_subsciptions', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->foreignId('subscription_plan_id')->constrained();
+            $table->unsignedInteger('price');
+            $table->dateTime('expired_date')->nullable();
+            $table->string('payment_status', 10)->default('text');
+            $table->string('snapToken')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_subsciptions');
+    }
+};
