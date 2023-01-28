e811d4e703194fe61b3d14b1d0abdd66c73cc05c
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-28 23:22:39 +0700

    Feat: Integration Browse Movies

--- resources/js/Pages/User/Dashboard/Index.jsx
+++ resources/js/Pages/User/Dashboard/Index.jsx
@@ -46,13 +46,13 @@ export default function Dashboard({ auth, featuredMovies, movies }) {
                     Browse
                 </div>
                 <Flickity className="gap-[30px]" options={flickityOptions}>
-                    {[1, 2, 3, 4, 5, 6].map((i) => (
+                    {movies.map((movie) => (
                         <MovieCard
-                            key={i}
-                            slug="the-batman-in-love"
-                            name={`The Batman In Love ${i}`}
-                            category="Comedy"
-                            thumbnail="https://picsum.photos/id/1/300/300"
+                            key={movie.id}
+                            slug={movie.slug}
+                            name={movie.name}
+                            category={movie.category}
+                            thumbnail={movie.thumbnail}
                         />
                     ))}
                 </Flickity>
