ec74cf95dcc8ad1e238fc197c1efe4f53e2fb2c1
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-31 01:28:13 +0700

    Feat: Integration Subscription Detail Component 2

--- resources/js/Layouts/Authenticated/Sidebar.jsx
+++ resources/js/Layouts/Authenticated/Sidebar.jsx
@@ -161,24 +161,7 @@ export default function Sidebar() {
                     </div>
                     {/* End Others */}
 
-                    <SubscriptionDetail />
-
-                    {/* Start Subscription details */}
-                    {/* <div className="mt-auto pr-[30px]">
-                        <div className="p-5 bg-black rounded-[25px]">
-                            <img src="/icons/ic_star-rounded.svg" alt="" />
-                            <div className="text-white text-lg font-semibold mt-4 mb-8">
-                                For Greatest
-                            </div>
-                            <div className="text-white text-sm mb-2">
-                                5 of 30 days
-                            </div>
-                            <div className="rounded-full w-full h-[6px] bg-[#333333]">
-                                <div className="rounded-full h-full w-9/12 bg-alerange"></div>
-                            </div>
-                        </div>
-                    </div> */}
-                    {/* End Subscription details */}
+                    <SubscriptionDetail isPremium />
                 </div>
             </div>
         </aside>
--- resources/js/Layouts/Authenticated/SubscriptionDetail.jsx
+++ resources/js/Layouts/Authenticated/SubscriptionDetail.jsx
@@ -1,15 +1,44 @@
-export default function SubscriptionDetail() {
+export default function SubscriptionDetail(
+    isPremium,
+    remainingActiveDays,
+    activiteDays
+) {
     return (
-        <div className="mt-auto pr-[30px]">
-            <div className="p-5 bg-[#CCCCFF] rounded-[25px]">
-                <div className="text-black text-lg font-semibold mt-4 mb-8">
-                    Basic
+        <>
+            {/* BASIC */}
+            {!isPremium && (
+                <div className="mt-auto pr-[30px]">
+                    <div className="p-5 bg-[#CCCCFF] rounded-[25px]">
+                        <div className="text-black text-lg font-semibold mt-4 mb-8">
+                            Basic
+                        </div>
+                        <div className="text-black text-sm mb-2">
+                            5 of 30 days
+                        </div>
+                        <div className="rounded-full w-full h-[6px] bg-[#333333]">
+                            <div className="rounded-full h-full w-9/12 bg-alerange"></div>
+                        </div>
+                    </div>
                 </div>
-                <div className="text-black text-sm mb-2">5 of 30 days</div>
-                <div className="rounded-full w-full h-[6px] bg-[#333333]">
-                    <div className="rounded-full h-full w-9/12 bg-alerange"></div>
+            )}
+
+            {/* PREMIUM */}
+            {isPremium && (
+                <div className="mt-auto pr-[30px]">
+                    <div className="p-5 bg-black rounded-[25px]">
+                        <img src="/icons/ic_star-rounded.svg" alt="" />
+                        <div className="text-white text-lg font-semibold mt-4 mb-8">
+                            For Greatest
+                        </div>
+                        <div className="text-white text-sm mb-2">
+                            5 of 30 days
+                        </div>
+                        <div className="rounded-full w-full h-[6px] bg-[#333333]">
+                            <div className="rounded-full h-full w-9/12 bg-alerange"></div>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
+            )}
+        </>
     );
 }
