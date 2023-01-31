8786fc951742a55c153a8ec5418027489ae6cfb4
SetiawanChandra1 <wawantan17@gmail.com>
2023-01-31 15:44:02 +0700

    Feat: Integration Active Plan Global

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -5,6 +5,8 @@ namespace App\Http\Middleware;
 use Illuminate\Http\Request;
 use Inertia\Middleware;
 use Tightenco\Ziggy\Ziggy;
+use Auth;
+use Carbon\Carbon;
 
 class HandleInertiaRequests extends Middleware
 {
@@ -32,6 +34,24 @@ class HandleInertiaRequests extends Middleware
      * @param  \Illuminate\Http\Request  $request
      * @return mixed[]
      */
+
+    private function activePlan(){
+        $activePlan = Auth::user() ? Auth::user()->LastActiveSubcription : null;
+        if(!activePlan){
+            return null;
+        }
+
+        $lastDay = Carbon::parse($activePlan->updated_at)->addMonths($activePlan->subscriptionPlan->active_period_in_months);
+        $activeDays = Carbon::parse($activePlan->updated_at)->diffInDays($lastDay);
+        $remainingActiveDays = Carbon::parse($activePlan->expired_date)->diffInDays(Carbon::now());
+        
+        return[
+            'name' => $activePlan->subscriptionPlan->name,
+            'remainingActiveDays' => $remainingActiveDays,
+            'activeDays' => $activeDays, 
+        ];
+    }
+
     public function share(Request $request)
     {
         return array_merge(parent::share($request), [
